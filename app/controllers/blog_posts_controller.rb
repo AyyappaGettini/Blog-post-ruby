8784d82b5476d1487d6b33dd174c05f3af078118
AyyappaGettini <ayyappa.g@testyantra.in>
2025-01-20 16:14:25 +0530

    Created basic blog

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,60 @@
+class BlogPostsController<ApplicationController
+
+  before_action :authenticate_user!, except: [:show,:index]
+  before_action :set_blog_post, only: [:show,:edit,:update,:destory]
+
+  def index
+    @blog_posts=BlogPost.all
+  end
+
+  def show
+  end
+
+  def new
+    @blog_post=BlogPost.new
+  end
+
+  def create
+    @blog_post=BlogPost.new(blog_posts_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def update
+    if @blog_post.update(blog_posts_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destory
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
+  def set_blog_post
+    @blog_post=BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  # def authendicate_user!
+  #       redirect_to new_user_session_path, alert: "you must sigined in or sign up to continue" unless user_signed_in?
+  # end
+
+  private
+  def blog_posts_params
+    params.require(:blog_post).permit(:title,:body)
+  end
+
+
+end
